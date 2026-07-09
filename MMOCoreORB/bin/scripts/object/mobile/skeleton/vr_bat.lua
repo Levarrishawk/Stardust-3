6901c283279f9c61b03d6ecde8ee6ee9b833152d
Levarrishawk <levarrishawk@gmail.com>
2026-07-09 14:52:24 -0400

    added missing refs

--- MMOCoreORB/bin/scripts/object/mobile/skeleton/vr_bat.lua
+++ MMOCoreORB/bin/scripts/object/mobile/skeleton/vr_bat.lua
@@ -41,26 +41,8 @@
 --this exception also makes it possible to release a modified version 
 
 
+object_mobile_skeleton_vr_bat = object_mobile_skeleton_shared_vr_bat:new {
 
---Children folder includes
+}
 
-includeFile("custom_content/tangible/deed/serverobjects.lua")
-includeFile("custom_content/tangible/furniture/serverobjects.lua")
-includeFile("custom_content/tangible/painting/serverobjects.lua")
-includeFile("custom_content/tangible/wearables/serverobjects.lua")
---
-
-includeFile("custom_content/tangible/collection/serverobjects.lua")
-includeFile("custom_content/tangible/door/serverobjects.lua")
-includeFile("custom_content/tangible/dungeon/serverobjects.lua")
-includeFile("custom_content/tangible/holiday/serverobjects.lua")
-includeFile("custom_content/tangible/hologram/serverobjects.lua")
-includeFile("custom_content/tangible/item/serverobjects.lua")
---includeFile("custom_content/tangible/loot/serverobjects.lua")
-includeFile("custom_content/tangible/quest/serverobjects.lua")
-includeFile("custom_content/tangible/npe/serverobjects.lua")
-includeFile("custom_content/tangible/smuggler/serverobjects.lua")
-includeFile("custom_content/tangible/tcg/serverobjects.lua")
-includeFile("custom_content/tangible/veteran_reward/serverobjects.lua")
-
-
+ObjectTemplates:addTemplate(object_mobile_skeleton_vr_bat, "object/mobile/skeleton/vr_bat.iff")
