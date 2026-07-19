f46d8a874eb902be302d6b96fb6514e23004963e
Levarrishawk <levarrishawk@gmail.com>
2026-07-19 16:03:49 -0400

    [iteration] Disabled Jedi Progression

--- MMOCoreORB/bin/scripts/managers/jedi/jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/jedi_manager.lua
@@ -11,7 +11,8 @@ if _TEST then
 	ITEMTHEATERDATAPAD = 2
 end
 
-jediProgressionType = VILLAGEJEDIPROGRESSION
+jediProgressionType = NOJEDIPROGRESSION
+--jediProgressionType = VILLAGEJEDIPROGRESSION
 
 jediManagerName = "JediManager"
 
