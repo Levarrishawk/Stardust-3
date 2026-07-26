3e60907d70ed19c4c772fbaba82d64b46e0a8078
Levarrishawk <levarrishawk@gmail.com>
2026-07-26 14:16:17 -0400

    More Loadtime error bashing

--- MMOCoreORB/bin/scripts/object/custom_content/tangible/smuggler/contraband/objects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/tangible/smuggler/contraband/objects.lua
@@ -215,5 +215,11 @@ object_tangible_smuggler_contraband_shared_contraband_general_2 = SharedTangible
 
 ObjectTemplates:addClientTemplate(object_tangible_smuggler_contraband_shared_contraband_general_2, "object/tangible/smuggler/contraband/shared_contraband_general_2.iff")
 ------------------------------------------------------------------------------------------------------------------------------------
+object_tangible_smuggler_contraband_shared_contraband_base = SharedTangibleObjectTemplate:new {
+  clientTemplateFileName = "object/tangible/smuggler/contraband/shared_contraband_base.iff"
+}
+
+ObjectTemplates:addClientTemplate(object_tangible_smuggler_contraband_shared_contraband_base, "object/tangible/smuggler/contraband/shared_contraband_base.iff")
+------------------------------------------------------------------------------------------------------------------------------------
 
 
