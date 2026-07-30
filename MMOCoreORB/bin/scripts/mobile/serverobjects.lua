bd83b3a0a298c0a273f328a2c472895dd5d61178
Levarrishawk <levarrishawk@gmail.com>
2026-07-30 07:07:12 -0400

    Update serverobjects.lua

--- MMOCoreORB/bin/scripts/mobile/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/serverobjects.lua
@@ -46,5 +46,8 @@ includeFile("lair/serverobjects.lua")
 -- Outfits
 includeFile("outfits/serverobjects.lua")
 
+--Custom NPCs
+includeFile("custom_content/serverobjects.lua")
+
 -- Custom content - Loads last to allow for overrides
 includeFile("../custom_scripts/mobile/serverobjects.lua")
