815d1a2171f7fa4ad7645964ee866c784875ab16
Levarrishawk <levarrishawk@gmail.com>
2026-08-02 12:49:51 -0400

    Update isb_outfit.lua

--- MMOCoreORB/bin/scripts/mobile/outfits/isb_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/isb_outfit.lua
@@ -5,10 +5,10 @@ isb_outfit = {
      -- {"/private/index_color_facial_hair", 21}
     },
 
-    {objectTemplate = "object/tangible/wearables/jacket/jacket_s03.iff", customizationVariables = {{"/private/index_color_1", 20}} },
-    {objectTemplate = "object/tangible/wearables/pants/pants_s15.iff", customizationVariables = {{"/private/index_color_1", 92}} },
-    {objectTemplate = "object/tangible/wearables/hat/hat_imp_s01.iff", customizationVariables = {{"/private/index_color_1", 92}} },
-    {objectTemplate = "object/tangible/wearables/boots/boots_14.iff", customizationVariables = {{"/private/index_color_1", 97}} }   
+    {objectTemplate = "object/tangible/wearables/jacket/jacket_s03.iff", customizationVariables = {{"/private/index_color_1", 42}} },
+    {objectTemplate = "object/tangible/wearables/pants/pants_s15.iff", customizationVariables = {{"/private/index_color_1", 41}} },
+    {objectTemplate = "object/tangible/wearables/hat/hat_imp_s01.iff", customizationVariables = {{"/private/index_color_1", 42}} },
+    {objectTemplate = "object/tangible/wearables/boots/boots_s14.iff", customizationVariables = {{"/private/index_color_1", 41}} }   
   }
 }
 
