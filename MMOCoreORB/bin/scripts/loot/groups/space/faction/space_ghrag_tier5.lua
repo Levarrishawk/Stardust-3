d07107aae87522a890f4093a06326945a6819f5b
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-08-05 09:38:16 -0600

    [space-port] loot (new): space_ghrag tier3/4/5 faction loot groups

---
+++ MMOCoreORB/bin/scripts/loot/groups/space/faction/space_ghrag_tier5.lua
@@ -0,0 +1,12 @@
+space_ghrag_tier5 = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{groupTemplate = "ship_component_pirate_tier4", weight = 2000000},
+		{groupTemplate = "ship_component_pirate_tier5", weight = 3000000},
+		{groupTemplate = "ship_component_blacksun_tier5", weight = 5000000}
+	}
+}
+
+addLootGroupTemplate("space_ghrag_tier5", space_ghrag_tier5)
