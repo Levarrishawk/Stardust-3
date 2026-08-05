f83ba152575642bc5f566376fff39cc41fdf72e2
Archon08 <51976358+Archon08@users.noreply.github.com>
2026-08-05 09:38:15 -0600

    [space-port] object (new): advanced_xwing + tieinterceptor_imperial_guard PCD/player/deed, 18 ace-pilot necklaces

---
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/necklace/ith_necklace_ace_pilot_neutral_f.lua
@@ -0,0 +1,29 @@
+--Copyright (C) 2010 <SWGEmu>
+
+
+--This File is part of Core3.
+
+--This program is free software; you can redistribute 
+--it and/or modify it under the terms of the GNU Lesser 
+--General Public License as published by the Free Software
+--Foundation; either version 2 of the License, 
+--or (at your option) any later version.
+
+--This program is distributed in the hope that it will be useful, 
+--but WITHOUT ANY WARRANTY; without even the implied warranty of 
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+--See the GNU Lesser General Public License for
+--more details.
+
+--You should have received a copy of the GNU Lesser General 
+--Public License along with this program; if not, write to
+--the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+-- Kessel Master 'ace pilot' reward wearable -- Ithorian mesh variant (Zina FAQ v3.0b VI).
+object_tangible_wearables_necklace_ith_necklace_ace_pilot_neutral_f = object_tangible_wearables_necklace_shared_ith_necklace_ace_pilot_neutral_f:new {
+	playerRaces = { "object/creature/player/ithorian_male.iff",
+				"object/creature/player/ithorian_female.iff" },
+}
+
+ObjectTemplates:addTemplate(object_tangible_wearables_necklace_ith_necklace_ace_pilot_neutral_f, "object/tangible/wearables/necklace/ith_necklace_ace_pilot_neutral_f.iff")
