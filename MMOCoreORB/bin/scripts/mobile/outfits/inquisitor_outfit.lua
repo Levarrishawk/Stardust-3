f0e6ff418558798890d5b879d474720e1e665dbc
Levarrishawk <levarrishawk@gmail.com>
2026-08-06 13:28:07 -0400

    Update inquisitor_outfit.lua

--- MMOCoreORB/bin/scripts/mobile/outfits/inquisitor_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/inquisitor_outfit.lua
@@ -7,8 +7,9 @@ inquisitor_outfit = {
 
     {objectTemplate = "object/tangible/wearables/bodysuit/bodysuit_tie_fighter.iff", customizationVariables = {{"/private/index_color_1", 1}} },
     {objectTemplate = "object/tangible/wearables/vest/vest_s11.iff", customizationVariables = {{"/private/index_color_1", 90}} },        
-    --{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_r.iff", customizationVariables = {{"/private/index_color_1", 1}, {"/private/index_color_2", 7}} },
+    {objectTemplate = "object/tangible/wearables/armor/marauder/armor_marauder_helmet_s02.iff", customizationVariables = {{"/private/index_color_1", 1}, {"/private/index_color_2", 2}} },
     {objectTemplate = "object/tangible/wearables/gloves/gloves_s02.iff", customizationVariables = {{"/private/index_color_1", 159}} },
+    {objectTemplate = "object/tangible/wearables/belt/belt_s01.iff", customizationVariables = {{"/private/index_color_1", 1}, {"/private/index_color_2", 2}} },
     {objectTemplate = "object/tangible/wearables/boots/boots_s14.iff", customizationVariables = {{"/private/index_color_1", 31}} }     
   }
 }
