3f27572ee8e3b04c62d8204a59ef25b38636c464
Levarrishawk <levarrishawk@gmail.com>
2026-08-06 13:55:08 -0400

    Update inquisitor_outfit.lua

--- MMOCoreORB/bin/scripts/mobile/outfits/inquisitor_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/inquisitor_outfit.lua
@@ -7,9 +7,9 @@ inquisitor_outfit = {
 
     {objectTemplate = "object/tangible/wearables/bodysuit/bodysuit_tie_fighter.iff", customizationVariables = {{"/private/index_color_1", 1}} },
     {objectTemplate = "object/tangible/wearables/vest/vest_s11.iff", customizationVariables = {{"/private/index_color_1", 90}} },        
-    {objectTemplate = "object/tangible/wearables/armor/ris/armor_ris_helmet.iff", customizationVariables = {{"/private/index_color_1", 158}, {"/private/index_color_2", 159}} },
-    {objectTemplate = "object/tangible/wearables/gloves/gloves_s02.iff", customizationVariables = {{"/private/index_color_1", 159}} },
-    {objectTemplate = "object/tangible/wearables/belt/belt_s01.iff", customizationVariables = {{"/private/index_color_1",159}} },
+    {objectTemplate = "object/tangible/wearables/armor/ris/armor_ris_helmet.iff", customizationVariables = {{"/private/index_color_1", 158}, {"/private/index_color_2", 158}} },
+    {objectTemplate = "object/tangible/wearables/gloves/gloves_s02.iff", customizationVariables = {{"/private/index_color_1", 158}} },
+    {objectTemplate = "object/tangible/wearables/belt/belt_s01.iff", customizationVariables = {{"/private/index_color_1",158}} },
     {objectTemplate = "object/tangible/wearables/boots/boots_s14.iff", customizationVariables = {{"/private/index_color_1", 31}} }     
   }
 }
