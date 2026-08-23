f85e303c864d3c980015d6a70b8d8bd3ac2fa7a1
Levarrishawk <levarrishawk@gmail.com>
2026-08-23 12:15:20 -0400

    Update fa_zoll.lua

--- MMOCoreORB/bin/scripts/mobile/space/fa_zoll.lua
+++ MMOCoreORB/bin/scripts/mobile/space/fa_zoll.lua
@@ -23,7 +23,7 @@ fa_zoll = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = JTLINTERESTING,
+	optionsBitmask = AIENABLED + JTLINTERESTING + CONVERSABLE,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/space_imperial_tier2_naboo.iff"},
