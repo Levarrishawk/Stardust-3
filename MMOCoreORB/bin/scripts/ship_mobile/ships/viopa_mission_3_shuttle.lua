a64f40c82fa349381e5cfccc4329a37e7597fc3f
Levarrishawk <levarrishawk@gmail.com>
2026-08-23 10:34:38 -0400

    Update viopa_mission_3_shuttle.lua

--- MMOCoreORB/bin/scripts/ship_mobile/ships/viopa_mission_3_shuttle.lua
+++ MMOCoreORB/bin/scripts/ship_mobile/ships/viopa_mission_3_shuttle.lua
@@ -14,8 +14,9 @@ viopa_mission_3_shuttle = ShipAgent:new {
 
 	aggressive = 0,
 
-	spaceFaction = "imperial",
-	alliedFactions = {"rsf"},
+	spaceFaction = "pirate",
+	alliedFactions = {"pirate"},
+	enemyFactions = {"imperial", "rebel", "civilian", "merchant", "rsf", "corsec", "hutt", "valarian"},
 	imperialFactionReward = -25,
 	rebelFactionReward = 13,
 	appearance = "imperial_officer",
