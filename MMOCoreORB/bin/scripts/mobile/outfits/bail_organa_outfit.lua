eef0806e266da0df97ca43fab277313f16254d7e
Levarrishawk <levarrishawk@gmail.com>
2026-09-23 10:18:38 -0400

    Update bail_organa_outfit.lua

--- MMOCoreORB/bin/scripts/mobile/outfits/bail_organa_outfit.lua
+++ MMOCoreORB/bin/scripts/mobile/outfits/bail_organa_outfit.lua
@@ -2,14 +2,17 @@ bail_organa_outfit = {
 	{
 		creatureCustomizationVariables = {
 			{"/shared_owner/index_color_skin", 12},
+			{"/shared_owner/blend_fat", 0},
+			{"/shared_owner/blend_skinny", 96},
+			{"/private/index_color_2", 1},
 			{"/private/index_style_beard", 0},
-			{"/private/index_color_facial_hair", 14}
+			{"/private/index_color_facial_hair", 2}
 		},
 
 		{objectTemplate = "object/tangible/wearables/robe/robe_s12.iff", customizationVariables = {{"/private/index_color_1", 243}, {"/private/index_color_2", 3}} },
 		{objectTemplate = "object/tangible/wearables/pants/pants_s12.iff", customizationVariables = {} },
 		{objectTemplate = "object/tangible/wearables/shoes/shoes_s01.iff", customizationVariables = {} },
-		{objectTemplate = "object/tangible/hair/human/hair_human_male_s07.iff", customizationVariables = {{"/private/index_color_1", 14}} }
+		{objectTemplate = "object/tangible/hair/human/hair_human_male_s07.iff", customizationVariables = {{"/private/index_color_1", 2}} }
 	}
 }
 
